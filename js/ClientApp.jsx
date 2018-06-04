3cf9d46d8e3d8368f7e9d528cb4aa3595014bf98
Mahima Rao <mahima.m23@gmail.com>
2018-06-04 13:43:04 -0700

    webpack

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
  
-const CE = React.createElement;
+const ce = React.createElement;
+
 const MyTitle = function(props) {
-  return CE("div", null, CE("h1", { style: { color: props.color } }, props.title));
+  return ce("div", null, ce("h1", { style: { color: props.color } }, props.title));
 };
+
 const Component = function() {
-  return CE("div",{ id: "Component" },
-    CE(MyTitle, { title: "Star Trek", color: "YellowGreen" }),
-    CE(MyTitle, { title: "Stranger Things", color: "GreenYellow" }),
-    CE(MyTitle, { title: "Rick and Morty", color: "DodgerBlue" }),
-    CE(MyTitle, { title: "Firefly", color: "Peru" })
+  return ce("div",{ id: "Component" },
+    ce(MyTitle, { title: "Star Trek", color: "YellowGreen" }),
+    ce(MyTitle, { title: "Stranger Things", color: "GreenYellow" }),
+    ce(MyTitle, { title: "Rick and Morty", color: "DodgerBlue" }),
+    ce(MyTitle, { title: "Firefly", color: "Peru" })
   );
 };
-ReactDOM.render(CE(Component), document.getElementById("app"));
+
+render(ce(Component), document.getElementById("app"));
 
 //jsx transpiles to createElement calls.
