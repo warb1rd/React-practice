4ab19520c0663df95766c693981290a9aae8e6ab
Mahima Rao <mahima.m23@gmail.com>
2018-06-05 15:57:57 -0700

    handler

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import preload from "../data.json";
 import ShowCard from "./ShowCard";
 
-const Search = () => (
+class Search extends Component{
+
+		state = {
+			searchTerm: ""
+		}
+	
+	handleSearchTermChange = (event) => {
+		this.setState({
+			searchTerm: event.target.value
+		})
+	}
+
+	render(){
+		return(
 	<div className="search">
+		<header>
+			<h1>Moving Pictures</h1>
+			<input 
+				onChange={this.handleSearchTermChange} 																			
+				value={this.state.searchTerm} 
+				type="text" 
+				placeholder="Search"/>
+		</header>
 		<div>
 			{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
 		</div>
 	</div>
-);
+		)
+	}
+}
 
 export default Search;
+
+// Creates a new function everytime it renders-.bind
